40c3037cfa62aa415e2413cd100874f535c1d85c
Joel Mueller <joelmueller27@gmail.com>
2025-03-14 17:34:15 +0100

    Improvement of open.sh

--- automation_scripts/open.py
+++ automation_scripts/open.py
@@ -3,29 +3,30 @@ import subprocess
 
 # Define a mapping of aliases to GitHub repository names
 REPO_MAP = {
-    "w": "hslu-weblab",
-    "q": "world-quiz",
+    "q": "hslu-intro-qc",
     "d": "devices",
     "s": "hslu-swat",
-    "p": "hslu-pcp",
-    "i": "hslu-intro-qc"
+    "p": "hslu-pcp"
 }
 
-# Check if an alias is provided
+OTHER_WEBSITES = {
+    "w": "https://github.com/qubegubble/caqtdm_opcua"
+}
+
 if len(sys.argv) < 2:
     print("Please provide an alias as an argument.")
     sys.exit(1)
 
-# Retrieve the full repository name using the alias
 alias = sys.argv[1]
 
 if alias in REPO_MAP:
     repo_name = REPO_MAP[alias]
+    url = f"https://github.com/Joel-Mueller/{repo_name}"
+elif alias in OTHER_WEBSITES:
+    url = OTHER_WEBSITES[alias]
 else:
-    repo_name = alias
+    url = f"https://github.com/Joel-Mueller/{alias}"
 
-
-url = f"https://github.com/Joel-Mueller/{repo_name}"
 
 subprocess.run(["open", url])
 
