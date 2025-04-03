dc7992d30f2d07137694b04b314d94d8a4e39ea2
Joel Mueller <joelmueller27@gmail.com>
2025-04-03 17:31:19 +0200

    Update

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,12 +1,12 @@
 # Installation of repo
 
-PATH_OF_CONFIGFILES="${HOME}/devices/config"
+PATH_OF_CONFIGFILES="${HOME}/devices/homebrew"
 
 # Install homebrew formulas
 echo "Install the homebrew formulas"
 read -p "Do you want to install the homebrew formulas? (y/n): " install_homebrew
 if [[ "$install_homebrew" =~ ^[Yy]$ ]]; then
-    xargs brew install < $PATH_OF_CONFIGFILES/homebrew_formulas.txt
+    xargs brew install < $PATH_OF_CONFIGFILES/formulas.txt
 else
   echo "Skipping homebrew formulas installation."
 fi
@@ -15,8 +15,7 @@ fi
 echo "Install the homebrew casks"
 read -p "Do you want to install the homebrew casks? (y/n): " install_casks
 if [[ "$install_casks" =~ ^[Yy]$ ]]; then
-    xargs brew install --cask < $PATH_OF_CONFIGFILES/cask_formulas.txt
+    xargs brew install --cask < $PATH_OF_CONFIGFILES/casks.txt
 else
   echo "Skipping homebrew casks installation."
 fi
-
--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,4 +1,3 @@
 PATH_OF_DOTFILES=$HOME/devices/dotfiles
-
 ls -A $PATH_OF_DOTFILES | xargs -I {} rm -f $HOME/{}
 ls -A $PATH_OF_DOTFILES | xargs -I {} ln -nfs $PATH_OF_DOTFILES/{} $HOME/{}
