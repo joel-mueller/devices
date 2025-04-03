36bcc65e5d22c0a606daedd2b641259fa87369b0
Joel Mueller <joelmueller27@gmail.com>
2025-04-03 23:34:25 +0200

    Move every note to the new notes Repository

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,14 +2,21 @@
 
 PATH_OF_DOTFILES=$HOME/devices/dotfiles
 ANTIGEN_PATH=$HOME/antigen.zsh
+ANTIGEN_REPO=$HOME/.antigen
 
 # Check if Antigen is already installed
 if [ -f "$ANTIGEN_PATH" ]; then
     rm "$ANTIGEN_PATH"
-    echo "Antigen is not installed. Installing now..."
-else
-    echo "Antigen is already installed."
+    echo "Uninstall Antigen file"
 fi
+
+if [ -f "$ANTIGEN_REPO" ]; then
+    rm -rf "$ANTIGEN_REPO"
+    echo "Uninstall Antigen file"
+fi
+
+# Installing Antigen
+echo "Installing Antigen"
 curl -L git.io/antigen > $HOME/antigen.zsh
 
 # Remove links to existing dotfiles
