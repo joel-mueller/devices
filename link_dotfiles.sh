4e666dc61513c35bf168a262857a8d2760370213
Joel Mueller <joelmueller27@gmail.com>
2025-04-03 16:45:45 +0200

    Update linkers

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,4 +1,6 @@
-ls $PATH_OF_DOTFILES | xargs -I {} rm -f $HOME/{}
-ls $PATH_OF_DOTFILES | xargs -I {} ln -nfs $PATH_OF_DOTFILES/{} $HOME/{}
+PATH_OF_DOTFILES=$HOME/devices/dotfiles
 
-source .zshrc
+ls -A $PATH_OF_DOTFILES | xargs -I {} rm -f $HOME/{}
+ls -A $PATH_OF_DOTFILES | xargs -I {} ln -nfs $PATH_OF_DOTFILES/{} $HOME/{}
+
+source $HOME/.zshrc
