7236c3859c881db9337aad9402168999e6f76100
Joel Mueller <joelmueller27@gmail.com>
2025-04-15 17:49:01 +0200

    Add zip to backup file

--- backup_repositories.sh
+++ backup_repositories.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-backup_dir="$HOME/backup_repositories_$(date -u +%d_%m_%Y)"
+dir_name="backup_repositories_$(date -u +%d_%m_%Y)"
+backup_dir="$HOME/$dir_name"
 
-mkdir $backup_dir
+mkdir "$backup_dir"
 
-cd $backup_dir
+cd "$backup_dir" || exit 1
 
-xargs -I {} git clone --mirror {} < $HOME/devices/backup/joel-mueller-repositories.txt
+xargs -I {} git clone --mirror {} < "$HOME/devices/backup/joel-mueller-repositories.txt"
+
+cd
+
+zip -r "${dir_name}.zip" "$dir_name"
